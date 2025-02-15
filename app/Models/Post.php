8376f8fdef729dba235b72d3525e80739c149b41
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-15 14:07:33 +0700

    sampai 6

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Models;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                
+            ]
+            ];
+    }
+}    
+
+?>
