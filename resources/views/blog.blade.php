5ab7595df562e501efdcfa9294161149406d3157
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-15 09:25:12 +0700

    Continue component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -117,13 +117,13 @@
   
     <header class="bg-white shadow-sm">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog</h1>
       </div>
     </header>
 
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p>Welcome home page</p>
+        <p>About Page</p>
       </div>
     </main>
   </div>  
