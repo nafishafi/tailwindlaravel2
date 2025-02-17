42e2ff48cc840b9e04a291e8d9cb02cc5c428208
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2025-02-17 11:13:02 +0700

    finished - 11

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,20 @@ namespace App\Models;
 
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model // tabel post
 {
+    use HasFactory;
     // protected $table = 'blog_posts'; // Jika nama tabel berbeda
     // protected $primaryKey = 'post_id'; // Jika nama primary key berbeda
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }    
 
 ?>
